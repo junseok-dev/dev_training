03fa02dae48a137b9fe5c2991f03b0e7fd5dd3a6
Park <eric676392@gmail.com>
2026-01-10 17:10:33 +0900

    feat: sql 실습 문제 풀이 추가

--- 02_practice/SQL/sql_practice01.sql
+++ 02_practice/SQL/sql_practice01.sql
@@ -1,28 +1,167 @@
-`homework_emp_251110.sql`에 작성하세요.
-
-```sql
--- 1. EMPLOYEE 테이블에서 이름 끝이 `연`으로 끝나는 사원의 이름을 출력하시오. 
+-- Active: 1762504522605@@127.0.0.1@3306@empdb
+-- Active: 1762504522605@@127.0.0.1@3306
+-- 1. EMPLOYEE 테이블에서 이름 끝이 `연`으로 끝나는 사원의 이름을 출력하시오. # 51
+SELECT EMP_NAME FROM EMPLOYEE WHERE EMP_NAME LIKE '%연';
 
 -- 2. EMPLOYEE 테이블에서 전화번호 처음 3자리가 `010`이 아닌 사원의 이름, 전화번호를 출력하시오.
+SELECT EMP_NAME, PHONE
+FROM EMPLOYEE
+WHERE
+    SUBSTR(PHONE, 1, 3) <> '010';
 
 -- 3. EMPLOYEE 테이블에서 메일주소 '_'의 앞이 4자이면서, DEPT_CODE가 `D9` 또는 `D5`이고 고용일이 `90/01/01` ~ `01/12/31`이면서, 월급이 270만원 이상인 사원의 전체 정보를 출력하시오.
+SELECT *
+FROM EMPLOYEE
+WHERE
+    LENGTH(
+        SUBSTR(
+            EMAIL,
+            1,
+            INSTR(EMAIL, '_') - 1
+        )
+    ) = 4
+    AND DEPT_CODE IN ('D9', 'D5')
+    AND HIRE_DATE BETWEEN TO_DATE ('1990-01-01', 'YYYY-MM-DD') AND TO_DATE  ('2001-12-31', 'YYYY-MM-DD')
+    AND SALARY >= 2700000;
 
 -- 4. EMPLOYEE 테이블에서 현재 근무 중인 사원을 이름 오름차순으로 정렬해서 출력하세요.
+SELECT EMP_NAME
+FROM EMPLOYEE
+WHERE
+    ENT_DATE IS NULL
+ORDER BY EMP_NAME ASC;
 
--- 5. 사원별 입사일, 퇴사일, 근무기간(일)을 조회하세요. 퇴사자 역시 조회되어야 합니다. 
-```
+-- 5. 사원별 입사일, 퇴사일, 근무기간(일)을 조회하세요. 퇴사자 역시 조회되어야 합니다.
+SELECT
+    EMP_NAME,
+    HIRE_DATE,
+    NVL (ENT_DATE, SYSDATE) AS ENT_DATE,
+    NVL (ENT_DATE, SYSDATE) - HIRE_DATE AS WORK_DAYS
+FROM EMPLOYEE;
 
-### grouping 처리
-```
+## grouping 처리
+
 -- 6. EMPLOYEE 테이블에서 여자사원의 급여 총 합을 계산
 -- (주민등록번호를 이용해 남여사원을 구분하세요.)
+SELECT SUM(SALARY) AS FEMALE_SALARY_SUM
+FROM EMPLOYEE
+WHERE
+    SUBSTR(EMP_NO, 8, 1) IN ('2', '4');
 
 -- 7. 부서코드가 D5인 사원들의 급여총합, 보너스 총합 조회
+SELECT SUM(SALARY) AS TOTAL_SALARY, SUM(SALARY * NVL (BONUS, 0)) AS TOTAL_BONUS
+FROM EMPLOYEE
+WHERE
+    DEPT_CODE = 'D5';
 
 -- 8. 부서코드가 D5인 직원의 보너스 포함 연봉의 총합 을 계산
+SELECT SUM(
+        SALARY * 12 * (1 + NVL (BONUS, 0))
+    ) AS TOTAL_ANNUAL_PAY
+FROM EMPLOYEE
+WHERE
+    DEPT_CODE = 'D5';
 
 -- 9. 남/여 사원 급여합계를 동시에 표현(가공된 컬럼의 합계)
+SELECT
+    SUM(
+        CASE
+            WHEN SUBSTR(EMP_NO, 8, 1) IN ('1', '3') THEN SALARY
+        END
+    ) AS MALE_SALARY_SUM,
+    SUM(
+        CASE
+            WHEN SUBSTR(EMP_NO, 8, 1) IN ('2', '4') THEN SALARY
+        END
+    ) AS FEMALE_SALARY_SUM
+FROM EMPLOYEE;
 
 -- 10.  EMPLOYEE 테이블에서 실제 사원들이 소속되어있는 부서의 수를 조회 (유령부서 제외)
 -- (NULL은 제외하고, 중복된 부서는 하나로 카운팅)
-```
+SELECT COUNT(DISTINCT DEPT_CODE) AS REAL_DEPT_COUNT
+FROM EMPLOYEE
+WHERE
+    DEPT_CODE IS NOT NULL;
+
+# 사원테이블 확인
+select * from employee;
+
+select * from department;
+
+select * from job;
+
+select * from location;
+
+select * from nation;
+
+select * from sal_grade;
+
+-- 5. 사원별 입사일, 퇴사일, 근무기간(일)을 조회하세요. 퇴사자 역시 조회되어야 합니다.
+SELECT emp_name, hire_date, quit_date, datediff(
+        ifnull(quit_date, now()), hire_date
+    )
+FROM employee;
+---------------------------------------------------------------
+
+-- 9. 남/여 사원 급여합계를 동시에 표현 (가공된 컬럼의 합계)
+-- 버전 1
+select
+    emp_name,
+    emp_no,
+    if(
+        substr(emp_no, 8, 1) = 1
+        or substr(emp_no, 8, 1) = 3,
+        '남',
+        '여'
+    ) gender,
+    if(
+        substr(emp_no, 8, 1) in (1, 3),
+        '남',
+        '여'
+    ) gender
+from employee;
+-------------------------------------------------------------------
+-- 버전 2
+select if(
+        substr(emp_no, 8, 1) in (1, 3), '남', '여'
+    ), format(sum(salary), 0) sum_salary
+from employee
+GROUP BY
+    if(
+        substr(emp_no, 8, 1) in (1, 3),
+        '남',
+        '여'
+    );
+-------------------------------------------------------------------
+-- 버전 3
+SELECT format(
+        sum(
+            if(
+                substr(emp_no, 8, 1) in (1, 3), salary, 0
+            )
+        ), 0
+    ) '남자사원 급여합계', format(
+        sum(
+            if(
+                substr(emp_no, 8, 1) in (1, 3), 0, salary
+            )
+        ), 0
+    ) '여자사원 급여합계'
+FROM employee;
+
+-- case 함수
+
+-- case함수
+SELECT
+    emp_name,
+    emp_no,
+    case
+        when substr(emp_no, 8, 1) in (1, 3) then '남'
+        when substr(emp_no, 8, 1) in (2, 4) then '여'
+    end as "gender", -- case type1방식 (if..elif 방식)
+    case substr(emp_no, 8, 1)
+        when "1" then "남"
+        when "3" then "남"
+        else "여"
+    end as "gender" -- case type2방식 (match..case 방식)
+FROM employee;
--- 02_practice/SQL/sql_practice02.sql
+++ 02_practice/SQL/sql_practice02.sql
@@ -1,28 +1,173 @@
-`homework_emp_251111.sql`파일에 작성하세요.
+-- Active: 1762504522605@@127.0.0.1@3306@empdb
+-- `homework_emp_251111.sql`파일에 작성하세요.
+-- select * from employee;
+-- ```
 
-```
-1. 2020년 12월 25일이 무슨 요일인지 조회하시오.
+-- 1. 2020년 12월 25일이 무슨 요일인지 조회하시오. # 60
+SELECT DAYNAME('2020-12-25') AS 요일;
 
-2. 주민번호가 1970년대생이면서 성별이 여자이고, 성이 전씨인 직원들의 사원명, 주민번호, 부서명, 직급명을 조회하시오.
+-- 2. 주민번호가 1970년대생이면서 성별이 여자이고, 성이 전씨인 직원들의 사원명, 주민번호, 부서명, 직급명을 조회하시오.
+SELECT e.EMP_NAME, e.EMP_NO, d.DEPT_TITLE, j.JOB_NAME
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+WHERE
+    SUBSTR(e.EMP_NO, 1, 2) BETWEEN '70' AND '79'
+    AND SUBSTR(e.EMP_NO, 8, 1) IN ('2', '4') -- 여성
+    AND e.EMP_NAME LIKE '전%';
 
-3. 가장 나이가 적은 직원의 사번, 사원명, 나이, 부서명, 직급명을 조회하시오.
+-- 3. 가장 나이가 적은 직원의 사번, 사원명, 나이, 부서명, 직급명을 조회하시오.
+-- 나이 계산 datediff(현재날짜 - 생년월일) / 365 
 
-4. 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
+-- 버전 1
+SELECT
+    emp_name,
+    emp_no,
+    truncate(
+        datediff(
+            now(), 
+            concat(if(substr(emp_no, 8, 1) in ('1', '2'), '19', '20'), substr(emp_no, 1, 6))
+        ) / 365
+    , 0) age
+FROM
+    employee;
 
-5. 해외영업부에 근무하는 사원명, 직급명, 부서코드, 부서명을 조회하시오.
+-- 버전 2
+SELECT
+    emp_id,
+    emp_name,
+    (SELECT job_name FROM job WHERE job_code = E.job_code) job_name,
+    (SELECT dept_title FROM department WHERE dept_id = E.dept_code) dept_title,
+    truncate(
+        datediff(
+            now(), 
+            concat(if(substr(emp_no, 8, 1) in ('1', '2'), '19', '20'), substr(emp_no, 1, 6))
+        ) / 365
+    , 0) age
+FROM
+    employee E
+ORDER BY
+    age
+LIMIT 
+    1;
 
-6. 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
+-- 버전 3
+SELECT e.EMP_ID, e.EMP_NAME, TIMESTAMPDIFF(
+        YEAR, STR_TO_DATE(
+            SUBSTR(e.EMP_NO, 1, 6), '%y%m%d'
+        ), CURDATE()
+    ) AS 나이, d.DEPT_TITLE, j.JOB_NAME
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+ORDER BY 나이 ASC
+LIMIT 1;
 
-7. 부서코드가 D2인 직원들의 사원명, 직급명, 부서명, 근무지역명을 조회하시오.
+-- 4. 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
+SELECT e.EMP_ID, e.EMP_NAME, d.DEPT_TITLE
+FROM EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+WHERE
+    e.EMP_NAME LIKE '%형%';
 
-8. 급여등급테이블 sal_grade의 등급별 최대급여(MAX_SAL)보다 많이 받는 직원들의 사원명, 직급명, 급여, 연봉을 조회하시오.    
-    (사원테이블과 급여등급테이블을 SAL_LEVEL컬럼기준으로 동등(등가) 조인할 것)
-    
-9. 한국(KO)과 일본(JP)에 근무하는 직원들의 사원명, 부서명, 지역명, 국가명을 조회하시오.
+-- 5. 해외영업부에 근무하는 사원명, 직급명, 부서코드, 부서명을 조회하시오.
+SELECT e.EMP_NAME, j.JOB_NAME, d.DEPT_ID, d.DEPT_TITLE
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+WHERE
+    d.DEPT_TITLE LIKE '해외영업%';
 
-10. 같은 부서에 근무하는 직원들의 사원명, 부서명, 동료이름을 조회하시오. (group_concat함수)
+-- 6. 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
+SELECT e.EMP_NAME, e.BONUS, d.DEPT_TITLE, l.LOCAL_NAME
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN LOCATION l ON d.LOCATION_ID = l.LOCAL_CODE
+WHERE
+    e.BONUS IS NOT NULL;
 
-11. 보너스포인트가 없는 직원들 중에서 직급이 차장과 사원인 직원들의 사원명, 직급명, 급여를 조회하시오. 단, join과 in 연산자 사용할 것
+-- 7. 부서코드가 D2인 직원들의 사원명, 직급명, 부서명, 근무지역명을 조회하시오.
+SELECT e.EMP_NAME, j.JOB_NAME, d.DEPT_TITLE, l.LOCAL_NAME
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+    JOIN LOCATION l ON d.LOCATION_ID = l.LOCAL_CODE
+WHERE
+    e.DEPT_CODE = 'D2';
 
-12. 재직중인 직원과 퇴사한 직원의 수를 조회하시오.
-```
+-- 8. 급여등급테이블 sal_grade의 등급별 최대급여(MAX_SAL)보다 많이 받는 직원들의 사원명, 직급명, 급여, 연봉을 조회하시오.
+--     (사원테이블과 급여등급테이블을 SAL_LEVEL컬럼기준으로 동등(등가) 조인할 것)
+SELECT e.EMP_NAME, j.JOB_NAME, e.SALARY, (e.SALARY * 12) AS 연봉
+FROM
+    EMPLOYEE e
+    JOIN SAL_GRADE s ON e.SAL_LEVEL = s.SAL_LEVEL
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+WHERE
+    e.SALARY > s.MAX_SAL;
+
+-- 9. 한국(KO)과 일본(JP)에 근무하는 직원들의 사원명, 부서명, 지역명, 국가명을 조회하시오.
+SELECT e.EMP_NAME, d.DEPT_TITLE, l.LOCAL_NAME, n.NATIONAL_NAME
+FROM
+    EMPLOYEE e
+    JOIN DEPARTMENT d ON e.DEPT_CODE = d.DEPT_ID
+    JOIN LOCATION l ON d.LOCATION_ID = l.LOCAL_CODE
+    JOIN NATION n ON l.NATIONAL_CODE = n.NATIONAL_CODE
+WHERE
+    n.NATIONAL_CODE IN ('KO', 'JP');
+
+-- 10. 같은 부서에 근무하는 직원들의 사원명, 부서명, 동료이름을 조회하시오. (group_concat함수)
+SELECT a.EMP_NAME AS 사원명, d.DEPT_TITLE AS 부서명, b.EMP_NAME AS 동료이름
+FROM
+    EMPLOYEE a
+    JOIN EMPLOYEE b ON a.DEPT_CODE = b.DEPT_CODE
+    AND a.EMP_ID <> b.EMP_ID
+    JOIN DEPARTMENT d ON a.DEPT_CODE = d.DEPT_ID
+ORDER BY a.EMP_NAME;
+
+-- 11. 보너스포인트가 없는 직원들 중에서 직급이 차장과 사원인 직원들의 사원명, 직급명, 급여를 조회하시오. 단, join과 in 연산자 사용할 것
+SELECT e.EMP_NAME, j.JOB_NAME, e.SALARY
+FROM EMPLOYEE e
+    JOIN JOB j ON e.JOB_CODE = j.JOB_CODE
+WHERE
+    e.BONUS IS NULL
+    AND j.JOB_NAME IN ('차장', '사원');
+
+-- 12. 재직중인 직원과 퇴사한 직원의 수를 조회하시오.
+-- ```
+
+-- 버전 1
+select    
+    if(QUIT_yn='Y', '재직자', '퇴사자') 재직여부,
+    count(*) 
+from
+    employee
+group BY
+    quit_yn;
+
+-- 버전 2
+SELECT
+    if(isnull(quit_date), '재직', '퇴사') 재직여부,
+    count(*) 수
+FROM
+    employee
+GROUP BY
+    if(isnull(quit_date), '재직', '퇴사');
+
+-- 버전 3
+SELECT SUM(
+        CASE
+            WHEN e.QUIT_YN = 'N' THEN 1
+            ELSE 0
+        END
+    ) AS 재직중, SUM(
+        CASE
+            WHEN e.QUIT_YN = 'Y' THEN 1
+            ELSE 0
+        END
+    ) AS 퇴사자
+FROM EMPLOYEE e;
